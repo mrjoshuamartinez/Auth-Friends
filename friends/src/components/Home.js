32ee0fe0d22a99c5f561a99c7c91543b157d7c6c
Joshua Martinez <mrjoshuamartinez@gmail.com>
2020-11-06 02:30:57 -0500

    Added Logout Option

--- friends/src/components/Home.js
+++ friends/src/components/Home.js
@@ -2,10 +2,30 @@
 import React from "react";
 import Friends from "./Friends";
 import Header from './Header';
+import { useHistory } from 'react-router-dom';
+import '../App.css';
 
 export default function FriendsHomePage() {
+    const history = useHistory();
     return (
     <>
+    <div className="span-hover">
+        <span
+            style={{
+                fontSize: '15px',
+                color: '#61dafb',
+                fontWeight: '700',
+                fontFamily: 'Tahoma',
+                cursor: 'pointer'
+            }}
+            onClick={() => {
+                localStorage.removeItem("token");
+                history.push("/login")
+            }}
+        >
+            Logout
+        </span>
+    </div>
         <Header />
         <Friends />
     </>
